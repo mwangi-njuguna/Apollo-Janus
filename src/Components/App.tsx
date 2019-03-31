437473f38393878b314b17171418bef06843fcb6
mwangi-njuguna <mwanginjuguna59@gmail.com>
2019-04-01 00:07:26 +0300

    Add apollo client configuration

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../Styles/App.scss';
+import '../Styles/App.sass';
 
 class App extends Component {
   render() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Styles/index.scss';
+import './Styles/index.sass';
 import App from './Components/App';
+
+// Apollo client configs imports
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const rootNode = document.getElementById('root');
 
-ReactDOM.render(<App />, rootNode);
+//TODO: change the uri incase you want to run on a prod environment
+const httpLink = createHttpLink({
+  uri: 'http:localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  rootNode
+);
