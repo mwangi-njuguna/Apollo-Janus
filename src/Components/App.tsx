9b6b61ca31e840730bc52865d1318b21014b1fa1
mwangi-njuguna <mwanginjuguna59@gmail.com>
2019-03-31 23:12:40 +0300

    Create initial folder structure

---
+++ src/Components/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../Styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Styles/index.scss';
+import App from './Components/App';
+
+const rootNode = document.getElementById('root');
+
+ReactDOM.render(<App />, rootNode);
