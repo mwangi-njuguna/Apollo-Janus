bd05be5b2323b7eaf3a67d0ec5192a8b39b398ae
mwangi-njuguna <mwanginjuguna59@gmail.com>
2019-04-01 00:07:26 +0300

    Add apollo client configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Styles/index.scss';
+import './Styles/index.sass';
 import App from './Components/App';
+
+// Apollo client configs imports
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const rootNode = document.getElementById('root');
 
-ReactDOM.render(<App />, rootNode);
+//TODO: change the uri incase you want to run on a prod environment
+// The uri points to graphql server instance uri
+const serverLink = createHttpLink({
+  uri: 'http:localhost:4000'
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: serverLink,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  rootNode
+);
